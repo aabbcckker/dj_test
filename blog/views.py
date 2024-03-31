75e1b29cba809e91ef7f2837a63a0d3196381d3c
zqw <484395832@qq.com>
2024-03-31 10:01:20 +0800

    进一步填充blog/view.py的函数和俩个html的代码，页面初具雏形

--- blog/views.py
+++ blog/views.py
@@ -4,19 +4,22 @@ from .models import Category, Post, Tag
 # Create your views here.
 
 def post_list(request, category_id=None, tag_id=None):
-    if tag_id:
-        try:
-            tag = Tag.objects.get(id=tag_id)
-        except Tag.DoesNotExist:
-            post_list = []
-        else:
-            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
-    else:
-        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
-        if category_id:
-            post_list = post_list.filter(category_id=category_id)
+    tag = None
+    category = None
 
-    return render(request, 'blog/list.html', context={'post_list': post_list})
+    if tag_id:
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
+    else:
+        post_list = Post.latest_posts()
+
+    context = {
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+    }
+    return render(request, 'blog/list.html', context=context)
 
 
 def post_detail(request, post_id):
