d46d506f293761815d1772ab1da154aa1efb7305
zqw <484395832@qq.com>
2024-04-08 17:09:51 +0800

    完善、修改了侧边栏

--- config/admin.py
+++ config/admin.py
@@ -4,15 +4,20 @@ from typeidea.base_admin import BaseOwnerAdmin
 # Register your models here.
 
 @admin.register(Link)
-class LinkAdmin(BaseOwnerAdmin):
+class LinkAdmin(admin.ModelAdmin):
     list_display = ('title', 'href', 'status', 'weight', 'created_time')
     fields = ('title', 'href', 'status', 'weight')
-
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(LinkAdmin, self).save_model(request, obj, form, change)
 
 
 @admin.register(Sidebar)
-class SidebarAdmin(BaseOwnerAdmin):
+class SidebarAdmin(admin.ModelAdmin):
     list_display = ('title', 'type', 'content', 'created_time')
     fields = ('title', 'type', 'content')
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(SidebarAdmin, self).save_model(request, obj, form, change)
 
 
